9b86588271f3dfcb337e0e348f0ab0be6b588f94
Dan Wyman <dwyman@slingtechnologies.com>
2015-10-29 21:14:32 +0000

    Added config override file so that settings can be changed without editing main script

--- tooquiet.sh
+++ tooquiet.sh
@@ -10,6 +10,8 @@
 
 ######################################################
 ## SETTINGS
+## These are the defaults.  Do not edit.  To customize settings,
+## see override-example.txt.
 
 testMode=0 # 0 for normal operation, or 1 to disable shutdown and echo instead of emailing
 email="example@domain.com" # This person gets "shutting down" emails
@@ -19,6 +21,13 @@ maxMinutesApacheInactive=60
 apacheAccessLog="/var/log/apache2/access.log"
 tempFile="/tmp/tooquiet.tmp"
 defaultMinutesCommandLineInactive="999" # Used if can not determine inactivty time
+
+# Load custom settings, if any.
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+if [ -f ${dir}/override.txt ]
+then
+    . ${dir}/override.txt
+fi
 
 
 ######################################################
